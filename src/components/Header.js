bd78816354c6b062507c1cfd90635c2e957dc355
Chris Wong <chris.wong.ksl@gmail.com>
2025-03-01 10:54:44 +0800

    Create todo

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { createTodo } from '../features/slices/todoSlice'
+
+function Header() {
+  const dispatch = useDispatch()
+  const [name, setName] = useState('')
+
+  function handleChange(e) {
+    setName(() => e.target.value)
+  }
+
+  function handleSubmit(e) {
+    if (e.key === 'Enter') {
+      dispatch(createTodo({name}))
+      setName(() => '')
+    }
+  }
+  return (
+    <header className='header'>
+      <h1>todos</h1>
+      <input
+        className='new-todo'
+        placeholder='What needs to be done?'
+        value={name}
+        onInput={handleChange}
+        onKeyUp={handleSubmit}
+      />
+    </header>
+  )
+}
+
+export default Header
--- src/features/slices/todoSlice.js
+++ src/features/slices/todoSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { v4 as uuidv4 } from 'uuid'
 
 const initialState = {
   todos: [],
@@ -12,9 +13,13 @@ const todoSlice = createSlice({
       state.todos = [...action.payload.todos]
       return state
     },
+    createTodo: (state, action) => {
+      state.todos = [...state.todos, { id: uuidv4(), name: action.payload.name, completed: false }]
+      return state
+    },
   },
 })
 
 export const selectTodos = (state) => state.todos
-export const { loadTodo } = todoSlice.actions
-export default todoSlice.reducer 
+export const { loadTodo, createTodo } = todoSlice.actions
+export default todoSlice.reducer
