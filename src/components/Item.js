0756b0cf6b3d061edab6d20074bf86001da1a48c
Chris Wong <chris.wong.ksl@gmail.com>
2025-03-01 14:01:35 +0800

    Focus when double click

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,8 +1,8 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 function Item(props) {
   const [editing, setEditing] = useState(false)
-  const [todo, setTodo] = useState({...props.todo})
+  const [todo, setTodo] = useState({ ...props.todo })
   const inputRef = useRef(null)
 
   function handleCompleted() {
@@ -10,10 +10,10 @@ function Item(props) {
     setTodo((pre) => {
       return {
         ...pre,
-        completed
+        completed,
       }
     })
-    props.update({id: todo.id, completed })
+    props.update({ id: todo.id, completed })
   }
 
   function handleEdit() {
@@ -24,13 +24,44 @@ function Item(props) {
     props.remove(todo.id)
   }
 
+  function handleChange(e) {
+    setTodo((pre) => {
+      return {
+        ...pre,
+        name: e.target.value,
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (editing) {
+      inputRef.current.focus();
+    }
+  }, [editing]);
+
   return (
-    <li className={[todo.completed ? "completed": "", editing ? "editing": ""].join("") }>
-      <div className="view">
-        <input className="toggle" type="checkbox" checked={todo.completed} onChange={handleCompleted} />
-        <label onDoubleClick={handleEdit}>{ todo.name }</label>
-        <button className="destroy" onClick={handleRemove} />
+    <li
+      className={[
+        todo.completed ? 'completed' : '',
+        editing ? 'editing' : '',
+      ].join(' ')}
+    >
+      <div className='view'>
+        <input
+          className='toggle'
+          type='checkbox'
+          checked={todo.completed}
+          onChange={handleCompleted}
+        />
+        <label onDoubleClick={handleEdit}>{todo.name}</label>
+        <button className='destroy' onClick={handleRemove} />
       </div>
+      <input
+        className="edit"
+        ref={inputRef}
+        value={todo.name}
+        onChange={handleChange}
+      />
     </li>
   )
 }
